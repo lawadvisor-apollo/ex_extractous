ccbb87623eb8930199eb63c090be6c2072ad3bd2
Joshua Poyatos <joshua.poyatos@lawadvisor.com>
2025-05-30 18:15:35 +0800

    Update env

--- lib/ex_extractous.ex
+++ lib/ex_extractous.ex
@@ -6,7 +6,7 @@ defmodule ExExtractous do
     crate: "ex_extractous",
     targets: ~w(x86_64-unknown-linux-musl),
     base_url: "https://github.com/lawadvisor-apollo/ex_extractous/releases/download/v#{version}",
-    force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
+    force_build: System.get_env("EX_EXTRACTOUS_BUILD") in ["1", "true"],
     version: version
 
   def extract(_path, _as_xml), do: :erlang.nif_error(:nif_not_loaded)
